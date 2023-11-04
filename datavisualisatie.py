64da0db032f871a8f8d61d029c1cdd4d15b57d65
Haikevt <71081163+Haikevt@users.noreply.github.com>
2023-11-04 21:44:02 +0100

    plotjes voor trends halen uit features

--- datavisualisatie.py
+++ datavisualisatie.py
@@ -1,1 +1,11 @@
 # TODO visualiseer de data en haal info uit de dataset zodat we daar iets over kunnen schrijven
+df = pd.read_csv('/content/drive/MyDrive/iml.csv', on_bad_lines='skip', sep=',', index_col=0)
+df_price = df['price']
+df = df.drop(columns=['id', 'sqft_lot15', 'price'])
+
+for feature in df:
+  plt.scatter(df[str(feature)], df_price, color = 'magenta')
+  plt.title('Random forest Model')
+  plt.xlabel(str(feature))
+  plt.ylabel('price')
+  plt.show()
